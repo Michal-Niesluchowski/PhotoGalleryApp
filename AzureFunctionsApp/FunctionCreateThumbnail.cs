2a3d101caf65c4834d14df63296a38d8a067f2f0
Michal Niesluchowski <m.niesluchowski@protonmail.com>
2021-06-10 17:52:39 +0200

    Azure function create thumbnail WIP

--- AzureFunctionsApp/FunctionCreateThumbnail.cs
+++ AzureFunctionsApp/FunctionCreateThumbnail.cs
@@ -1,15 +1,26 @@
 using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace AzureFunctionsApp
 {
     public static class FunctionCreateThumbnail
     {
+        private static readonly int _imageHeight = 200;
+        private static readonly int _imageWidth = 200;
+
         [FunctionName("FunctionCreateThumbnail")]
-        public static void Run([BlobTrigger("photogallery-images/{name}", Connection = "")]Stream myBlob, string name, TraceWriter log)
+        public static void Run([BlobTrigger("photogallery-images/{name}", Connection = "")]
+            Stream myBlob, string name, TraceWriter log)
         {
-            log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            using (Image image = Image.Load(myBlob))
+            {
+                image.Mutate(x => x.Resize(_imageWidth, _imageHeight));
+                
+                log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n  Size: {myBlob.Length} Bytes");
+            }
         }
     }
 }
